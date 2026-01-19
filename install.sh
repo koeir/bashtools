e5c225667a6978d01ca7f0cda2a08a58b1a70560
koeir <kianjohnban@gmail.com>
2026-01-19 23:32:29 +0800

    log: holy crap.

--- install.sh
+++ install.sh
@@ -10,16 +10,17 @@ if (( EUID != 0 )); then
     exit -1
 fi
 
-installation_dir="/usr/share/bashtools"
-if [ ! -d "$installation_dir" ]; then
-    echo "[$0] Making directory \"${installation_dir}\"..."
-    mkdir -p "$installation_dir"
+# Do not change for the love of god
+bashtools_installation_dir="/usr/share/bashtools"
+if [ ! -d "$bashtools_installation_dir" ]; then
+    echo "[$0] Making directory \"${bashtools_installation_dir}\"..."
+    mkdir -p "$bashtools_installation_dir"
 
-    export PATH="${PATH}:${installation_dir}"
+    export PATH="${PATH}:${bashtools_installation_dir}"
 fi
 
 if [[ $1 != "-y" ]]; then
-    read -p "[$0] Copy the executables to "${installation_dir}"? (y/n): " confirm
+    read -p "[$0] Copy the executables to "${bashtools_installation_dir}"? (y/n): " confirm
     if [[ ! $confirm =~ ^[yY]$ ]]; then
         echo -e "[$0] Cancelling..."
         exit 0
@@ -44,19 +45,19 @@ while IFS= read -r -d '' tool; do
     installed=false        
     filename="$(echo "$tool" | awk -F '/' '{print $NF}')"
    
-    if [ -f "${installation_dir}/${filename}" ]; then
+    if [ -f "${bashtools_installation_dir}/${filename}" ]; then
         
-        if diff -q "$tool" "${installation_dir}/${filename}"; then
+        if diff -q "$tool" "${bashtools_installation_dir}/${filename}"; then
 
             installed=true
             skipped=$((skipped+1))
 
         else
             
-            echo -e "${RED}[$0] ! Filename \"${filename}\" found in \"${installation_dir}\" !..."
+            echo -e "${RED}[$0] ! Filename \"${filename}\" found in \"${bashtools_installation_dir}\" !..."
             echo -e "...but the contents are different."
-            echo -e "${GREEN}[$0] Making a copy of \"${installation_dir}/${filename}\" to /tmp...${NORMAL}"
-            cp "${installation_dir}/${filename}" /tmp
+            echo -e "${GREEN}[$0] Making a copy of \"${bashtools_installation_dir}/${filename}\" to /tmp...${NORMAL}"
+            cp "${bashtools_installation_dir}/${filename}" /tmp
             updated=$((updated+1))
 
         fi
@@ -65,8 +66,8 @@ while IFS= read -r -d '' tool; do
     if [[ $installed == true ]]; then
         echo "[${0}] \"$filename\" already installed."
     else
-        echo "[$0] Copying \"${tool}\" to \"${installation_dir}\"..."
-        cp "$tool" "${installation_dir}/${filename}"
+        echo "[$0] Copying \"${tool}\" to \"${bashtools_installation_dir}\"..."
+        cp "$tool" "${bashtools_installation_dir}/${filename}"
     fi
     
 done < <(find "$tools_dir" -type f -executable -print0);
@@ -102,6 +103,7 @@ else
     cp "$bashtools_exec" "${installation_dir}/$tool"
 fi
 
+chmod 751 -R "$bashtools_installation_dir"
 
 echo
 echo "[${0}] Done."
