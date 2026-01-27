872ebf79e2eddcbdcb3fec73969f7b552fb0bd19
koeir <kianjohnban@gmail.com>
2026-01-27 14:52:10 +0800

    log: improved installation script logic

--- install.sh
+++ install.sh
@@ -48,46 +48,58 @@ fi
 
 skipped=0
 updated=0
+installed=0
+installtool() {
+    if [ -z "$2" ]; then
+        local installation_dir="${BASHTOOLS_INSTALLATION_DIR}"
+    else
+        local installation_dir="$2"
+    fi
 
-# Find files in $tools_dir directory
-# put them in the installation directory
-# the tools are accessible via the interface "bashtools"
-while IFS= read -r -d '' tool; do
+    local alreadyInstalled=false        
+    local tool="$1"
 
-    installed=false        
     filename="$(echo "$tool" | awk -F '/' '{print $NF}')"
-   
-    if [ -f "${BASHTOOLS_INSTALLATION_DIR}/${filename}" ]; then
-        
-        if diff -q "$tool" "${BASHTOOLS_INSTALLATION_DIR}/${filename}"; then
 
-            installed=true
+    if [ -f "${installation_dir}/${filename}" ]; then
+        
+        if diff -q "$tool" "${installation_dir}/${filename}"; then
+
+            alreadyInstalled=true
             skipped=$((skipped+1))
 
         else
             
-            echo -e "${RED}[$0] ! Filename \"${filename}\" found in \"${BASHTOOLS_INSTALLATION_DIR}\" !..."
+            echo -e "${RED}[$0] ! Filename \"${filename}\" found in \"${installation_dir}\" !..."
             echo -e "...but the contents are different."
-            echo -e "${GREEN}[$0] Making a copy of \"${BASHTOOLS_INSTALLATION_DIR}/${filename}\" to /tmp...${NORMAL}"
-            cp "${BASHTOOLS_INSTALLATION_DIR}/${filename}" /tmp
+            echo -e "${GREEN}[$0] Making a copy of \"${installation_dir}/${filename}\" to /tmp...${NORMAL}"
+            cp "${installation_dir}/${filename}" /tmp
             updated=$((updated+1))
 
         fi
     fi
     
-    if [[ $installed == true ]]; then
+    if [[ $alreadyInstalled == true ]]; then
         echo "[$0] \"$filename\" already installed."
     else
 
-        echo "[$0] Copying \"${tool}\" to \"${BASHTOOLS_INSTALLATION_DIR}\"..."
-        cp "$tool" "${BASHTOOLS_INSTALLATION_DIR}/${filename}"
+        echo "[$0] Copying \"${tool}\" to \"${installation_dir}\"..."
+        cp "$tool" "${installation_dir}/${filename}"
+        installed=$((installed+1))
 
     fi
-    
+}
+
+while IFS= read -r -d '' tool; do
+    installtool "$tool"
 done < <(find "$tools_dir" -type f -executable -print0);
+
+bshtls="${bashtools}/bshtls"
+installtool "$bshtls" "/usr/bin"
 
 echo ""
 echo "[$0] Done."
+echo "[$0] Installed:            "${installed}""
 echo "[$0] Overwritten files:    "${updated}""
 echo "[$0] Skipped files:        "${skipped}""
 echo ""
